009d2d8b8a724757fb2325275c4840173a6d204b
Yamnel <yamnel@users.noreply.github.com>
2017-03-10 11:54:59 -0500

    Settings page, and Main menu edits, resizing of images and buttons

--- A&ELoginScreen/RegisterScreenViewController.swift
+++ A&ELoginScreen/RegisterScreenViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class RegisterScreenViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     
@@ -53,8 +55,12 @@ class RegisterScreenViewController: UIViewController, UIPickerViewDelegate, UIPi
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
         self.view.endEditing(true)
     }
     
+    // Cancel Landscape Orientation 
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+
 }
