cb50a7ee5dec6979878a99acb864d3e790118b37
Yamnel <yamnel@users.noreply.github.com>
2017-03-16 13:59:30 -0400

    Fixed login class name.

--- A&ELoginScreen/LoginController.swift
+++ A&ELoginScreen/LoginController.swift
@@ -18,7 +18,7 @@ extension UINavigationController {
 }
 
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
     
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
